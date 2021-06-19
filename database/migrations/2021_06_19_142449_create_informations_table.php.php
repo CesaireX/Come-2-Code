95106b158af619b5677033ed8fe714dc41b2d8e7
Cooleliel <elielcoulibaly39@gmail.com>
2021-06-19 16:07:12 +0000

    Creations des migrations. Creations des models. Mise a jour du dossier vendor

---
+++ database/migrations/2021_06_19_142449_create_informations_table.php.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInformationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('informations', function (Blueprint $table) {
+            $table->string('idCc')->unique();
+            $table->string('idA')->unique();
+            $table->string('objet');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
